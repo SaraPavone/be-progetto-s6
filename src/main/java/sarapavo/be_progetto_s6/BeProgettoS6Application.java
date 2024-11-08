fa7f20103687b0fb64307b1583b033c0019f6a53
SaraPavone <172259730+SaraPavone@users.noreply.github.com>
2024-11-08 09:20:17 +0100

    first commit

---
+++ src/main/java/sarapavo/be_progetto_s6/BeProgettoS6Application.java
@@ -0,0 +1,13 @@
+package sarapavo.be_progetto_s6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeProgettoS6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeProgettoS6Application.class, args);
+	}
+
+}
---
+++ src/test/java/sarapavo/be_progetto_s6/BeProgettoS6ApplicationTests.java
@@ -0,0 +1,13 @@
+package sarapavo.be_progetto_s6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProgettoS6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
